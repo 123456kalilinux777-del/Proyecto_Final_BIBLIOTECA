9c434309557492fa969e700b29c395917dd1eba5
123456kalilinux777-del <123456kalilinux777@gmail.com>
2026-05-27 00:18:56 -0400

    Codigo inicial de prueba del proyecto

---
+++ BIBLIOTECA.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+    using namespace std;
+
+    int main (){
+
+
+        cout << "HOLA GRUPO, ESPERO DESARROLLAR JUNTOS EL CODIGO DE LA BIBLIOTECA :D"<<endl;
+
+        system ("pause");
+        return 0;
+    }
